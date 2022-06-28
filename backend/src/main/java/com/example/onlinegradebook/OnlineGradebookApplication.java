22a62b4f71c8ebb93ec42c82ab5944d0007c84b8
unknown <adelina.mancheva@gmail.com>
2022-06-28 20:49:48 +0300

    fix create user

--- backend/src/main/java/com/example/onlinegradebook/OnlineGradebookApplication.java
+++ backend/src/main/java/com/example/onlinegradebook/OnlineGradebookApplication.java
@@ -3,8 +3,12 @@ package com.example.onlinegradebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = "com.example.onlinegradebook.model")
+//@EntityScan(basePackages = "com.example.onlinegradebook.model")
+@EntityScan(basePackages = "com.example.onlinegradebook")
+@EnableJpaRepositories(basePackages = "com.example.onlinegradebook")
 @SpringBootApplication()
 public class OnlineGradebookApplication {
 
